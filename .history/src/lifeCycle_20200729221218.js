872cb0cdea828cd8355cd5195fd11301fe1c74c2
Kimwill <67475953@qq.com>
2020-07-29 22:14:37 +0800

    test git

--- .history/src/lifeCycle_20200729221218.js
+++ .history/src/lifeCycle_20200729221218.js
@@ -14,15 +14,15 @@ export function lifeCycleMixin(Vue) {
   }
 }
 
-// export function mountComponent(vm, el) {
-//   function updateComponent() {
-//     // _render => 调用otps上的render方法，返回虚拟DOM
-//     // _update => 将虚拟DOM变为真实DOM
-//     vm._update(vm._render())
-//   }
-//   // 数据变化时，执行updateComponent更新视图
-//   new Watch(vm, updateComponent, () => {}, true)
-// }
+export function mountComponent(vm, el) {
+  function updateComponent() {
+    // _render => 调用otps上的render方法，返回虚拟DOM
+    // _update => 将虚拟DOM变为真实DOM
+    vm._update(vm._render())
+  }
+  // 数据变化时，执行updateComponent更新视图
+  new Watch(vm, updateComponent, () => {}, true)
+}
 
 function testGit() {
   console.log('1')
